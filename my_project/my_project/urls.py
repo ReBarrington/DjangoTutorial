de3e57e5356fff5234dde226155266ee7344afbb
Reagan Barrington <reagan@barringtonfamily.net>
2020-09-25 14:55:39 -0400

    about

--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    # links to url file inside blog directory
+    # links to url file inside blog directory, sending an empty string to blog.urls
 ]
