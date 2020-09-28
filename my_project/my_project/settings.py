490c27813ce9ebb90e65feee6b8f3ce3c6b6da77
Reagan Barrington <reagan@barringtonfamily.net>
2020-09-28 15:24:54 -0400

    successfully saved secret in .env

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -11,6 +11,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Project Base Paths
+# project_root/your_app/settings.py - 2 = project_root/
+ROOT_DIR = environ.Path(__file__) - 2
+
+env = environ.Env()
+
+env_file = ROOT_DIR('.env') # assume it is in project_root
+env.read_env(env_file)
+
+SECRET_KEY = env.str('DJANGO_SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,9 +30,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0u&_ft^6kxrt1zav!d)aqyn5=db#=4d3bpelx!$22fh-t3==bb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
