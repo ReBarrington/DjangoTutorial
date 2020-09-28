68f4eb14f29c5c06279fffe05926ea7d0b317ec8
Reagan Barrington <reagan@barringtonfamily.net>
2020-09-28 15:57:13 -0400

    looping over dummy data

--- my_project/blog/views.py
+++ my_project/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'ReaganBarrington',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 26, 2020',
+    },
+        {
+        'author': 'TeddyDickerson',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 28, 2020',
+    },
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
