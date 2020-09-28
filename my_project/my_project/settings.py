e6e1da2a366d65b3e960da366ee6c5efae0e016e
Reagan Barrington <reagan@barringtonfamily.net>
2020-09-28 15:13:53 -0400

    .env

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': ebv.db(),
+    'default': env.db(),
 }
 
 
