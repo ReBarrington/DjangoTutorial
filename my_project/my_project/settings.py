3b00592af7d286776fd4565d8d2e3ce58a4284cf
Reagan Barrington <reagan@barringtonfamily.net>
2020-10-02 09:49:08 -0400

    crispy forms

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
