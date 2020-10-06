68e0d96691f6c0148c0f6de9463aa83239db2abd
Reagan Barrington <reagan@barringtonfamily.net>
2020-10-06 15:33:52 -0400

    logout template

--- my_project/users/views.py
+++ my_project/users/views.py
@@ -8,9 +8,9 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'Your account has been created. You are now able to log in.')
             # blog-home is the name that we gave to the url pattern
-            return redirect('blog-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
